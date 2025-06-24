474b3482606773340648556392a7e904670621f5
Kasnix <luc.kasnix@gmail.com>
2025-06-24 17:35:46 -0300

    feat: add ComicCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
+"use client";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { useRouter } from "next/navigation";
 
-const Home = () => (
-  <Grid container direction="column" alignItems="center">
-    <Typography variant="h1">Hyse Comics</Typography>
-  </Grid>
-);
+import { ComicCard, type ComicCardProps } from "~/components/comic-card";
+import thumbnail from "~/storage/metaverse-cavalry-thumbnail.webp";
+
+const metaverseCavalryComic: Omit<ComicCardProps, "onReadButtonClick"> = {
+  id: "1",
+  title: "Metaverse Cavalry",
+  description:
+    "In a vast medieval world set within the Metaverse, a young warrior is summoned to face a looming threat that corrupts the very fabric of this digital reality: a powerful demon bent on destruction.",
+  image: {
+    src: thumbnail.src,
+    altText: "Metaverse Cavalry thumbnail",
+  },
+};
+
+const Home = () => {
+  const router = useRouter();
+
+  const handleReadButtonClick = (id: string) => {
+    router.push(`/comic?id=${id}`);
+  };
+
+  return (
+    <Grid container direction="column" alignItems="center">
+      <Typography variant="h1">Hyse Comics</Typography>
+      <ComicCard
+        {...metaverseCavalryComic}
+        onReadButtonClick={handleReadButtonClick}
+      />
+    </Grid>
+  );
+};
 
 export default Home;
---
+++ src/components/comic-card.tsx
@@ -0,0 +1,51 @@
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export type ComicCardProps = Readonly<{
+  id: string;
+  title: string;
+  description: string;
+  image: {
+    src: string;
+    altText: string;
+  };
+  onReadButtonClick: (id: string) => void;
+}>;
+
+export const ComicCard = ({
+  id,
+  title,
+  description,
+  image,
+  onReadButtonClick,
+}: ComicCardProps) => (
+  <Card sx={{ width: 320 }}>
+    <CardMedia
+      sx={{ width: 320, height: 240 }}
+      image={image.src}
+      title={image.altText}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5">
+        {title}
+      </Typography>
+      <Typography variant="body2" sx={{ color: "text.secondary" }}>
+        {description}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button
+        size="small"
+        onClick={() => {
+          onReadButtonClick(id);
+        }}
+      >
+        Read
+      </Button>
+    </CardActions>
+  </Card>
+);
