fa5824dcf437a7f86026ed90aed6823184054e6a
Kasnix <luc.kasnix@gmail.com>
2025-06-27 15:09:10 -0300

    feat: add primary color to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,4 +3,12 @@ import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
   cssVariables: true,
+  palette: {
+    primary: {
+      main: "#00796b",
+      light: "#339388",
+      dark: "#00544a",
+      contrastText: "#ffffff",
+    },
+  },
 });
