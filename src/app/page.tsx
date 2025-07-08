c24aa347945e0e5e2ba1776177627f8dbdbb7f4c
Kasnix <luc.kasnix@gmail.com>
2025-07-08 19:13:59 -0300

    feat: add ComicHighlightsSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,13 @@
-"use client";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { useRouter } from "next/navigation";
 
-import { ComicCard } from "~/components/comic-card";
-import { comicsMock } from "~/mocks/comics";
+import { ComicHighlightsSection } from "~/sections/comic-highlights-section";
 
-const HomePage = () => {
-  const router = useRouter();
-
-  const handleReadButtonClick = (id: string) => {
-    router.push(`/comic?id=${id}`);
-  };
-
-  return (
-    <Grid container direction="column" alignItems="center">
-      <Typography variant="h1">Hyse Comics</Typography>
-      <ComicCard
-        id={comicsMock[0].id}
-        title={comicsMock[0].title}
-        description={comicsMock[0].description}
-        image={{
-          src: comicsMock[0].imageUrl,
-          altText: comicsMock[0].imageAltText,
-        }}
-        onReadButtonClick={handleReadButtonClick}
-      />
-    </Grid>
-  );
-};
+const HomePage = () => (
+  <Grid container direction="column" alignItems="center">
+    <Typography variant="h1">Hyse Comics</Typography>
+    <ComicHighlightsSection />
+  </Grid>
+);
 
 export default HomePage;
