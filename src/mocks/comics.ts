65f0b01ceaad06198ae8ebca410ef8cddc08345b
Kasnix <luc.kasnix@gmail.com>
2025-10-13 11:47:21 -0300

    refactor: use images from Supabase Storage for comics

--- src/mocks/comics.ts
+++ src/mocks/comics.ts
@@ -1,8 +1,3 @@
-import metaverseCavalryPanel1 from "~/storage/metaverse-cavalry-panel-1.webp?url";
-import metaverseCavalryPanel2 from "~/storage/metaverse-cavalry-panel-2.webp?url";
-import metaverseCavalryPanel3 from "~/storage/metaverse-cavalry-panel-3.webp?url";
-import metaverseCavalryPanel4 from "~/storage/metaverse-cavalry-panel-4.webp?url";
-import metaverseCavalryThumbnail from "~/storage/metaverse-cavalry-thumbnail.webp?url";
 import type { Comic, ComicPanel } from "~/types/comics";
 
 export const comicsMock: Array<Comic> = [
@@ -14,8 +9,21 @@ export const comicsMock: Array<Comic> = [
     title: "Metaverse Cavalry",
     summary:
       "In a vast medieval world set within the Metaverse, a young warrior is summoned to face a looming threat that corrupts the very fabric of this digital reality: a powerful demon bent on destruction.",
-    imageUrl: metaverseCavalryThumbnail,
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/thumbnail.webp",
     imageAltText: "Metaverse Cavalry thumbnail",
+  },
+  {
+    id: "eAvdmZnEgri3",
+    structure: "single-panel",
+    orientation: "landscape",
+    genres: ["action", "adventure", "mystery"],
+    title: "Forbidden Treasures",
+    summary:
+      "In a world where ancient civilizations left secrets buried in forgotten temples, a young archaeologist embarks on risky expeditions in search of relics that could change the fate of humanity.",
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/thumbnail.webp",
+    imageAltText: "Forbidden Treasures thumbnail",
   },
 ];
 
@@ -23,7 +31,8 @@ export const comicPanelsMock: Array<ComicPanel> = [
   {
     id: "79e80cb6-5d66-49c6-a350-f2efd013ae63",
     comicId: "hsGYfEuPQH-U",
-    imageUrl: metaverseCavalryPanel1,
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/panel-1.webp",
     imageWidth: 1080,
     imageHeight: 720,
     imageAltText: "Metaverse Cavalry panel 1",
@@ -31,7 +40,8 @@ export const comicPanelsMock: Array<ComicPanel> = [
   {
     id: "925eb9c5-fe49-44bb-8c29-2495e0e3eb4f",
     comicId: "hsGYfEuPQH-U",
-    imageUrl: metaverseCavalryPanel2,
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/panel-2.webp",
     imageWidth: 1080,
     imageHeight: 720,
     imageAltText: "Metaverse Cavalry panel 2",
@@ -39,7 +49,8 @@ export const comicPanelsMock: Array<ComicPanel> = [
   {
     id: "5616800f-ed96-4995-b802-bb0ca3bed992",
     comicId: "hsGYfEuPQH-U",
-    imageUrl: metaverseCavalryPanel3,
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/panel-3.webp",
     imageWidth: 1080,
     imageHeight: 720,
     imageAltText: "Metaverse Cavalry panel 3",
@@ -47,9 +58,37 @@ export const comicPanelsMock: Array<ComicPanel> = [
   {
     id: "0d656322-0491-4d0a-a3db-f5393b57edc3",
     comicId: "hsGYfEuPQH-U",
-    imageUrl: metaverseCavalryPanel4,
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/panel-4.webp",
     imageWidth: 1080,
     imageHeight: 720,
     imageAltText: "Metaverse Cavalry panel 4",
+  },
+  {
+    id: "3e3ff617-908f-45cd-aa8d-096d7681de3c",
+    comicId: "eAvdmZnEgri3",
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/panel-1.webp",
+    imageWidth: 1080,
+    imageHeight: 720,
+    imageAltText: "Forbidden Treasures panel 1",
+  },
+  {
+    id: "9b6fdfb9-9ff1-4aa3-9a35-39462164e1ac",
+    comicId: "eAvdmZnEgri3",
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/panel-2.webp",
+    imageWidth: 1080,
+    imageHeight: 720,
+    imageAltText: "Forbidden Treasures panel 2",
+  },
+  {
+    id: "af693116-e36b-4718-9b1d-276eadf53d56",
+    comicId: "eAvdmZnEgri3",
+    imageUrl:
+      "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/panel-3.webp",
+    imageWidth: 1080,
+    imageHeight: 720,
+    imageAltText: "Forbidden Treasures panel 3",
   },
 ];
