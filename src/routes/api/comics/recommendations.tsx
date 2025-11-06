6e9c0a5668f8daefbc249ea698eec7d9397c108d
Kasnix <luc.kasnix@gmail.com>
2025-11-06 10:10:12 -0300

    refactor(api-comics): add chapter registration for recommendations

--- src/routes/api/comics/recommendations.tsx
+++ src/routes/api/comics/recommendations.tsx
@@ -3,20 +3,28 @@ import { json } from "@tanstack/react-start";
 
 import { comicChaptersMock, comicsMock } from "~/mocks/comics";
 import type {
-  Comic,
-  ComicChapter,
   ComicChapterWithComic,
   ComicRecommendation,
 } from "~/types/comics";
 
 const generateComicChaptersWithComic = (
-  comicChapters: Array<ComicChapter>,
-  comics: Array<Comic>,
+  chapterIds: Array<string>,
 ): Array<ComicChapterWithComic> => {
-  return comicChapters
-    .map((comicChapter) => {
-      const comic = comics.find((comic) => comic.id === comicChapter.comicId);
-      if (!comic) return null;
+  return chapterIds
+    .map((chapterId) => {
+      const comicChapter = comicChaptersMock.find(
+        (chapter) => chapter.id === chapterId,
+      );
+      if (!comicChapter) {
+        return null;
+      }
+      const comic = comicsMock.find(
+        (comic) => comic.id === comicChapter.comicId,
+      );
+      if (!comic) {
+        return null;
+      }
+
       return {
         ...comicChapter,
         comic,
@@ -30,21 +38,30 @@ export const Route = createFileRoute("/api/comics/recommendations")({
     handlers: {
       // TODO: Connect to Supabase and fetch data from there
       GET: async () => {
-        const comicChaptersWithComic = generateComicChaptersWithComic(
-          comicChaptersMock,
-          comicsMock,
-        );
-
         const comicRecommendations: Array<ComicRecommendation> = [
           {
             id: "aiEFHUOj",
             title: "Trending now",
-            items: comicChaptersWithComic,
+            items: generateComicChaptersWithComic([
+              "vNH8ZOb8",
+              "Uxg6G2c4",
+              "KoEb6BNw",
+              "cFCqsYye",
+              "dKtiDz0q",
+              "ocxHyVie",
+            ]),
           },
           {
             id: "JASVe5Aj",
             title: "Continue reading",
-            items: comicChaptersWithComic,
+            items: generateComicChaptersWithComic([
+              "vNH8ZOb8",
+              "Uxg6G2c4",
+              "KoEb6BNw",
+              "cFCqsYye",
+              "dKtiDz0q",
+              "ocxHyVie",
+            ]),
           },
         ];
 
