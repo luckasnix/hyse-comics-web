b147fdb037e6faee7239bd3ebe579a289ce9fac7
Kasnix <luc.kasnix@gmail.com>
2025-11-15 15:08:13 -0300

    feat(ComicRoute): add overview and navigation between chapters

--- src/routes/comics/$comicId.tsx
+++ src/routes/comics/$comicId.tsx
@@ -1,12 +1,57 @@
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import type { SxProps, Theme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import { createFileRoute } from "@tanstack/react-router";
+import { ComicChapterList } from "~/components/comic-chapter-list";
+import { ComicOverview } from "~/components/comic-overview";
 
-// TODO: Write route
-const ComicRoute = () => (
-  <main>
-    <h1>🚧 Under Construction</h1>
-  </main>
-);
+import { getChapters, getComic } from "~/services/comics";
+
+const containerStyle: SxProps<Theme> = {
+  paddingY: 4,
+};
+
+const ComicRoute = () => {
+  // TODO: Show an error component if there was one in the loader data
+  const { comic, chapters } = Route.useLoaderData();
+
+  const navigate = Route.useNavigate();
+
+  const navigateToChapter = (chapterId: string) => {
+    navigate({
+      to: "/chapters/$chapterId",
+      params: { chapterId: chapterId },
+    });
+  };
+
+  return (
+    <Container maxWidth="md" sx={containerStyle}>
+      <ComicOverview comic={comic} />
+      <Stack spacing={2}>
+        <Typography variant="h4">Chapters</Typography>
+        <ComicChapterList
+          chapters={chapters}
+          selectedChapterId={null}
+          onChapterClick={navigateToChapter}
+        />
+      </Stack>
+    </Container>
+  );
+};
 
 export const Route = createFileRoute("/comics/$comicId")({
   component: ComicRoute,
+  loader: async ({ params: { comicId } }) => {
+    // TODO: Handle errors that may occur in the request
+    const [comic, chapters] = await Promise.all([
+      getComic(comicId),
+      getChapters(comicId),
+    ]);
+
+    return {
+      comic,
+      chapters,
+    };
+  },
 });
