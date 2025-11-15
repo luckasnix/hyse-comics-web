cae516f78438aa5a4e7578ea31c71572f9ddaf7f
Kasnix <luc.kasnix@gmail.com>
2025-11-15 15:05:43 -0300

    fix(ComicOverview): adjust some styles

--- src/components/comic-overview.tsx
+++ src/components/comic-overview.tsx
@@ -1,13 +1,18 @@
 import Stack from "@mui/material/Stack";
+import type { SxProps, Theme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import type { CSSProperties } from "react";
 
 import type { Comic } from "~/types/comics";
 
+const containerStyle: SxProps<Theme> = {
+  marginBottom: 5,
+};
+
 const imageStyle: CSSProperties = {
   width: "auto",
-  maxWidth: "1200px",
-  height: "514px",
+  maxWidth: "852px",
+  height: "365px",
   borderRadius: "4px",
   objectFit: "cover",
 };
@@ -17,7 +22,7 @@ export type ComicOverviewProps = Readonly<{
 }>;
 
 export const ComicOverview = ({ comic }: ComicOverviewProps) => (
-  <Stack spacing={2}>
+  <Stack spacing={2} sx={containerStyle}>
     <img src={comic.coverUrl} alt={`${comic.title} cover`} style={imageStyle} />
     <Typography variant="h1">{comic.title}</Typography>
     <Typography variant="body1" color="text.secondary">
