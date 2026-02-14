33b5bc5feb7ac809c39fbe9b5bd011fddab2eb86
Kasnix <luc.kasnix@gmail.com>
2026-02-14 10:53:25 -0300

    refactor(comics): use Base62 for IDs

--- src/mocks/comics.ts
+++ src/mocks/comics.ts
@@ -2,7 +2,7 @@ import type { Chapter, Comic, Credit, Page } from "#/types/comics";
 
 export const comicsMock: Array<Comic> = [
   {
-    id: "iXSE",
+    id: "QhbGUrW2",
     structure: "single-panel",
     orientation: "landscape",
     direction: "eastern",
@@ -16,7 +16,7 @@ export const comicsMock: Array<Comic> = [
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/cv.webp",
   },
   {
-    id: "erZ7",
+    id: "LPLc5tsY",
     structure: "single-panel",
     orientation: "landscape",
     direction: "western",
@@ -30,7 +30,7 @@ export const comicsMock: Array<Comic> = [
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/cv.webp",
   },
   {
-    id: "FSOK",
+    id: "iEdqrCrJ",
     structure: "single-panel",
     orientation: "landscape",
     direction: "western",
@@ -44,7 +44,7 @@ export const comicsMock: Array<Comic> = [
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/skate-champion/cv.webp",
   },
   {
-    id: "lfUQ",
+    id: "t7uzBpFU",
     structure: "single-panel",
     orientation: "landscape",
     direction: "eastern",
@@ -58,7 +58,7 @@ export const comicsMock: Array<Comic> = [
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/blood-and-gold/cv.webp",
   },
   {
-    id: "tAip",
+    id: "9eaVmAst",
     structure: "single-panel",
     orientation: "landscape",
     direction: "western",
@@ -72,7 +72,7 @@ export const comicsMock: Array<Comic> = [
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/cv.webp",
   },
   {
-    id: "LAeD",
+    id: "4ShLAvTY",
     structure: "single-panel",
     orientation: "landscape",
     direction: "western",
@@ -89,64 +89,64 @@ export const comicsMock: Array<Comic> = [
 
 export const chaptersMock: Array<Chapter> = [
   {
-    id: "vNH8ZOb8",
-    comicId: "iXSE",
+    id: "3URztCal8w",
+    comicId: "QhbGUrW2",
     title: "The Knight's Departure",
     synopsis:
       "Clad in advanced armor, the warrior bids a heartfelt goodbye to the familiar smiles of the village, stepping into an unknown that lies beyond the tranquil community.",
   },
   {
-    id: "a6pJNagm",
-    comicId: "erZ7",
+    id: "77Pbvn95Tz",
+    comicId: "LPLc5tsY",
     title: "The Idol of Aztal'Kha",
     synopsis:
       "Amid the ruins, a hidden idol reveals its golden light, a discovery that may change everything, or unleash what was never meant to be found.",
   },
   {
-    id: "XM7ce9ji",
-    comicId: "erZ7",
+    id: "cIPaxxhdRk",
+    comicId: "LPLc5tsY",
     title: "The Jade Buddha",
     synopsis:
       "Deep within forgotten temples, a fearless archaeologist races against ancient traps to uncover a jade relic of untold power.",
   },
   {
-    id: "Uxg6G2c4",
-    comicId: "erZ7",
+    id: "8n86aCriNv",
+    comicId: "LPLc5tsY",
     title: "The Heart of Atlantis",
     synopsis:
       "Deep beneath the ocean, our heroine uncovers a legendary crystal. But her discovery awakens ancient forces guarding its secret.",
   },
   {
-    id: "KoEb6BNw",
-    comicId: "FSOK",
+    id: "mduaZ9Iu3k",
+    comicId: "iEdqrCrJ",
     title: "Rise Above",
     synopsis:
       "In front of a roaring crowd, one jump decides it all, the moment between falling short and becoming a legend.",
   },
   {
-    id: "cFCqsYye",
-    comicId: "lfUQ",
+    id: "h4Ot0UrDfB",
+    comicId: "t7uzBpFU",
     title: "Treasure Island",
     synopsis:
       "The crew lands on hostile shores, their captain leading the charge, where every wave brings the promise of blood and conquest.",
   },
   {
-    id: "dKtiDz0q",
-    comicId: "tAip",
+    id: "B7e2KQ40pw",
+    comicId: "9eaVmAst",
     title: "The Forest Trial",
     synopsis:
       "Lost in the dark woods, a young witch faces her first true test, where courage must burn brighter than fear.",
   },
   {
-    id: "awI4jabe",
-    comicId: "tAip",
+    id: "ppgL2CuVHR",
+    comicId: "9eaVmAst",
     title: "The Awakening of the Djinn",
     synopsis:
       "A curious young witch accidentally unleashes a powerful Djinn from an ancient spellbook. Now, she must face the consequences of her mistake.",
   },
   {
-    id: "ocxHyVie",
-    comicId: "LAeD",
+    id: "LlAkuYOs5J",
+    comicId: "4ShLAvTY",
     title: "The Midnight Exorcism",
     synopsis:
       "As the moon rises, faith is tested and fear takes form, only one will survive the night's unholy battle.",
@@ -155,8 +155,8 @@ export const chaptersMock: Array<Chapter> = [
 
 export const pagesMock: Array<Page> = [
   {
-    id: "79e80cb6-5d66-49c6-a350-f2efd013ae63",
-    chapterId: "vNH8ZOb8",
+    id: "hL12iPBkjxL1",
+    chapterId: "3URztCal8w",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/ch1-pg1.webp",
     imageWidth: 1080,
@@ -164,8 +164,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: null,
   },
   {
-    id: "925eb9c5-fe49-44bb-8c29-2495e0e3eb4f",
-    chapterId: "vNH8ZOb8",
+    id: "DudTXoHOKAYO",
+    chapterId: "3URztCal8w",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/ch1-pg2.webp",
     imageWidth: 1080,
@@ -173,8 +173,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: null,
   },
   {
-    id: "5616800f-ed96-4995-b802-bb0ca3bed992",
-    chapterId: "vNH8ZOb8",
+    id: "yzSRqK2NZKwW",
+    chapterId: "3URztCal8w",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/ch1-pg3.webp",
     imageWidth: 1080,
@@ -182,8 +182,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: null,
   },
   {
-    id: "0d656322-0491-4d0a-a3db-f5393b57edc3",
-    chapterId: "vNH8ZOb8",
+    id: "zNzTHIRtvxP2",
+    chapterId: "3URztCal8w",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/metaverse-cavalry/ch1-pg4.webp",
     imageWidth: 1080,
@@ -191,8 +191,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: null,
   },
   {
-    id: "3e3ff617-908f-45cd-aa8d-096d7681de3c",
-    chapterId: "a6pJNagm",
+    id: "F4dhc2moeLqL",
+    chapterId: "77Pbvn95Tz",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch1-pg1.webp",
     imageWidth: 1080,
@@ -200,8 +200,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "9b6fdfb9-9ff1-4aa3-9a35-39462164e1ac",
-    chapterId: "a6pJNagm",
+    id: "C4llozTOUTq6",
+    chapterId: "77Pbvn95Tz",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch1-pg2.webp",
     imageWidth: 1080,
@@ -209,8 +209,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "af693116-e36b-4718-9b1d-276eadf53d56",
-    chapterId: "a6pJNagm",
+    id: "g6Xibl9M3eCF",
+    chapterId: "77Pbvn95Tz",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch1-pg3.webp",
     imageWidth: 1080,
@@ -218,8 +218,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "c9ddd8f0-81be-484b-a15c-02caa73fade9",
-    chapterId: "XM7ce9ji",
+    id: "9qCb7Grf0Z3C",
+    chapterId: "cIPaxxhdRk",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch2-pg1.webp",
     imageWidth: 1080,
@@ -227,8 +227,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "bb6d1478-11fb-498e-b4b7-d32dfe06c425",
-    chapterId: "XM7ce9ji",
+    id: "WtUltVQkXfGD",
+    chapterId: "cIPaxxhdRk",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch2-pg2.webp",
     imageWidth: 1080,
@@ -236,8 +236,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "d83bad49-fc9b-41e8-9656-81cecff2132e",
-    chapterId: "XM7ce9ji",
+    id: "JXYWpZLLSsX8",
+    chapterId: "cIPaxxhdRk",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch2-pg3.webp",
     imageWidth: 1080,
@@ -245,8 +245,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "6fdb0ab6-275a-4ddb-985d-3fedc5fe9272",
-    chapterId: "Uxg6G2c4",
+    id: "HRGi1AmGMqIQ",
+    chapterId: "8n86aCriNv",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch3-pg1.webp",
     imageWidth: 1080,
@@ -254,8 +254,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "9478b810-e0c0-4974-a6cc-64d87c628050",
-    chapterId: "Uxg6G2c4",
+    id: "Z1q1ZsMl09zz",
+    chapterId: "8n86aCriNv",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch3-pg2.webp",
     imageWidth: 1080,
@@ -263,8 +263,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "adc5c686-4694-4670-b4d3-de8d63d67910",
-    chapterId: "Uxg6G2c4",
+    id: "V1UjEhS4zcfM",
+    chapterId: "8n86aCriNv",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/forbidden-treasures/ch3-pg3.webp",
     imageWidth: 1080,
@@ -272,8 +272,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "77834591-4e58-44e7-82fc-9fb5115638db",
-    chapterId: "KoEb6BNw",
+    id: "zXXAJX34uh5f",
+    chapterId: "mduaZ9Iu3k",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/skate-champion/ch1-pg1.webp",
     imageWidth: 1080,
@@ -281,8 +281,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "19d688a6-de37-476f-a73c-968fba98c389",
-    chapterId: "KoEb6BNw",
+    id: "4IsYMn6qYoVd",
+    chapterId: "mduaZ9Iu3k",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/skate-champion/ch1-pg2.webp",
     imageWidth: 1080,
@@ -290,8 +290,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "039fe50b-fa05-4c6e-a17e-cdafb6a821bc",
-    chapterId: "KoEb6BNw",
+    id: "SATeaYapvzwf",
+    chapterId: "mduaZ9Iu3k",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/skate-champion/ch1-pg3.webp",
     imageWidth: 1080,
@@ -299,8 +299,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "b0c5d640-cd94-4581-a54d-a7c8663a65ff",
-    chapterId: "cFCqsYye",
+    id: "MdBg2Xscgq6Q",
+    chapterId: "h4Ot0UrDfB",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/blood-and-gold/ch1-pg1.webp",
     imageWidth: 1080,
@@ -308,8 +308,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "parchment",
   },
   {
-    id: "290bfa89-f20d-450b-8997-987758c85b4f",
-    chapterId: "cFCqsYye",
+    id: "VBbOwxUVGWyY",
+    chapterId: "h4Ot0UrDfB",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/blood-and-gold/ch1-pg2.webp",
     imageWidth: 1080,
@@ -317,8 +317,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "parchment",
   },
   {
-    id: "c59adf66-2689-409c-9e6a-0a93963ee06c",
-    chapterId: "cFCqsYye",
+    id: "XinVfOSOlwAA",
+    chapterId: "h4Ot0UrDfB",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/blood-and-gold/ch1-pg3.webp",
     imageWidth: 1080,
@@ -326,8 +326,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "parchment",
   },
   {
-    id: "4a0bc7c5-0d7d-4d46-a187-721173acfc5d",
-    chapterId: "cFCqsYye",
+    id: "LzlhPril6R3S",
+    chapterId: "h4Ot0UrDfB",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/blood-and-gold/ch1-pg4.webp",
     imageWidth: 1080,
@@ -335,8 +335,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "parchment",
   },
   {
-    id: "b18c7dd8-a76d-485f-b217-5a23741c1b39",
-    chapterId: "dKtiDz0q",
+    id: "XTX0kqHXv3XG",
+    chapterId: "B7e2KQ40pw",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch1-pg1.webp",
     imageWidth: 1080,
@@ -344,8 +344,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "8ef69b6c-dc5a-4c26-bf43-bdd6c56a28f2",
-    chapterId: "dKtiDz0q",
+    id: "YdJZNZPhtiUX",
+    chapterId: "B7e2KQ40pw",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch1-pg2.webp",
     imageWidth: 1080,
@@ -353,8 +353,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "2ff8175c-bcd1-43b6-84ed-24b110be9a37",
-    chapterId: "dKtiDz0q",
+    id: "z1lTnhEqxxZG",
+    chapterId: "B7e2KQ40pw",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch1-pg3.webp",
     imageWidth: 1080,
@@ -362,8 +362,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "51a1d969-93b8-472a-a0c9-3add83c5e144",
-    chapterId: "awI4jabe",
+    id: "j9zFHiavtswx",
+    chapterId: "ppgL2CuVHR",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch2-pg1.webp",
     imageWidth: 1080,
@@ -371,8 +371,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "418c62bf-a9df-4cc9-b6b1-78f88cc75833",
-    chapterId: "awI4jabe",
+    id: "50Dbe0UEG9iy",
+    chapterId: "ppgL2CuVHR",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch2-pg2.webp",
     imageWidth: 1080,
@@ -380,8 +380,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "942c7860-3205-4f21-9ccb-4234e2e9b523",
-    chapterId: "awI4jabe",
+    id: "OFgYzDtf58Vk",
+    chapterId: "ppgL2CuVHR",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch2-pg3.webp",
     imageWidth: 1080,
@@ -389,8 +389,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "affc4cf6-c14f-433e-9472-28fc4c72e83b",
-    chapterId: "awI4jabe",
+    id: "Ks90KKvPVXKv",
+    chapterId: "ppgL2CuVHR",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch2-pg4.webp",
     imageWidth: 1080,
@@ -398,8 +398,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "2790db6f-3bc9-469e-9e57-9e2c63858b29",
-    chapterId: "awI4jabe",
+    id: "LnR7SZyywXaP",
+    chapterId: "ppgL2CuVHR",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/apprentice-of-witchcraft/ch2-pg5.webp",
     imageWidth: 1080,
@@ -407,8 +407,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "white-paper",
   },
   {
-    id: "5586aa59-1fae-4b5f-acb2-346ec0a487a8",
-    chapterId: "ocxHyVie",
+    id: "iLp24SwA5fBD",
+    chapterId: "LlAkuYOs5J",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/the-purifier/ch1-pg1.webp",
     imageWidth: 1080,
@@ -416,8 +416,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "black-paper",
   },
   {
-    id: "cc0472f3-fee5-42ce-8f41-fe01e9179062",
-    chapterId: "ocxHyVie",
+    id: "BYR877ZlOsZE",
+    chapterId: "LlAkuYOs5J",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/the-purifier/ch1-pg2.webp",
     imageWidth: 1080,
@@ -425,8 +425,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "black-paper",
   },
   {
-    id: "9a21c6dc-0edb-4c1d-ae95-b8c207dd24ab",
-    chapterId: "ocxHyVie",
+    id: "98SWWdBWqRmc",
+    chapterId: "LlAkuYOs5J",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/the-purifier/ch1-pg3.webp",
     imageWidth: 1080,
@@ -434,8 +434,8 @@ export const pagesMock: Array<Page> = [
     backgroundTexture: "black-paper",
   },
   {
-    id: "1843a560-6fa2-46de-8c28-9d3b1bfd1362",
-    chapterId: "ocxHyVie",
+    id: "euF02qcm8ZYU",
+    chapterId: "LlAkuYOs5J",
     imageUrl:
       "https://ncruxkkvlxobsrzmvbkz.supabase.co/storage/v1/object/public/comics/the-purifier/ch1-pg4.webp",
     imageWidth: 1080,
@@ -446,17 +446,17 @@ export const pagesMock: Array<Page> = [
 
 export const creditsMock: Array<Credit> = [
   {
-    chapterId: "vNH8ZOb8",
+    chapterId: "3URztCal8w",
     userId: "xhMmYL0qjhVK",
     role: "comics:writer",
   },
   {
-    chapterId: "KoEb6BNw",
+    chapterId: "mduaZ9Iu3k",
     userId: "xhMmYL0qjhVK",
     role: "comics:letterer",
   },
   {
-    chapterId: "ocxHyVie",
+    chapterId: "LlAkuYOs5J",
     userId: "xhMmYL0qjhVK",
     role: "comics:editor",
   },
