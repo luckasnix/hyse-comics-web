c455c4b8159c9d99fa6e7a220d6ff2b0f8266e03
Kasnix <luc.kasnix@gmail.com>
2026-03-10 10:37:44 -0300

    feat(constants-comics): add content warning labels

--- src/constants/comics.ts
+++ src/constants/comics.ts
@@ -1,4 +1,8 @@
-import type { AxisDirectionOptionType, ComicDirection } from "#/types/comics";
+import type {
+  AxisDirectionOptionType,
+  ComicDirection,
+  ContentWarning,
+} from "#/types/comics";
 
 export const comicReaderToolbarHeight = 56;
 
@@ -8,4 +12,12 @@ export const carouselDirectionFrom: Record<
 > = {
   western: "ltr",
   eastern: "rtl",
+};
+
+export const contentWarningLabelsFrom: Record<ContentWarning, string> = {
+  "ai-generated": "AI-Generated Content",
+  "graphic-violence": "Graphic Violence",
+  "sexual-content": "Sexual Content",
+  "strong-language": "Strong Language",
+  "substance-use": "Substance Use",
 };
