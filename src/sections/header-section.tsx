b6c3d3ed0890aa01c4efbd5dd8b989fae5d52c28
Kasnix <luc.kasnix@gmail.com>
2026-07-06 19:29:26 -0300

    feat(header-section): navigate home when logo is clicked

--- src/sections/header-section.tsx
+++ src/sections/header-section.tsx
@@ -1,6 +1,9 @@
 import Box from "@mui/material/Box";
 import type { SxProps, Theme } from "@mui/material/styles";
+import { Link, useParams } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
+
+import { linkResetStyle } from "#/styles/common";
 
 const containerStyle: SxProps<Theme> = {
   width: "100%",
@@ -11,9 +14,13 @@ const containerStyle: SxProps<Theme> = {
 export const HeaderSection = () => {
   const { t } = useTranslation();
 
+  const { locale } = useParams({ strict: false });
+
   return (
     <Box component="header" sx={containerStyle}>
-      <img src="/logo.svg" width={384} height={96} alt={t("brand.logoAlt")} />
+      <Link to="/{-$locale}" params={{ locale }} style={linkResetStyle}>
+        <img src="/logo.svg" width={384} height={96} alt={t("brand.logoAlt")} />
+      </Link>
     </Box>
   );
 };
